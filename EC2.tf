ad1a2614b90b583a996808616d6c8921f2223b96
vineet Bhardwaj <vineet.bhardwaj.hcl@sgx.com>
2024-05-06 11:41:52 +0800

    Revert "As a test removing last commit"
    This reverts commit 5ef10fb8bc96d3c9fe3400e99561d80a22b6088e.

--- EC2.tf
+++ EC2.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "server1"{
     instance_type = var.inc_type[0]
     subnet_id = var.subnet_id
     key_name = "mykey"
-    associate_public_ip_address = true
 
     tags = {
         Name = "labserver1"
